d20e5f7ab2e33ba94f6fd3d0a17166303178a39e
Vince Broz <vince@broz.cc>
2014-06-29 14:09:04 -0400

    Fix lack of module documentation

--- lib/quality/version.rb
+++ lib/quality/version.rb
@@ -1,3 +1,6 @@
+# Quality is a tool that runs quality checks on Ruby code using cane,
+# reek, flog and flay, and makes sure your numbers don't get any worse
+# over time.
 module Quality
   VERSION = '1.3.0'
 end
