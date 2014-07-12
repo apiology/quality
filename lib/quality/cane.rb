d16bd3cfd5050cd63f37bd1543ee08fc4994568a
Vince Broz <vince@broz.cc>
2014-07-12 17:34:23 -0400

    Document module for Rubocop

--- lib/quality/cane.rb
+++ lib/quality/cane.rb
@@ -1,3 +1,6 @@
+# Quality is a tool that runs quality checks on Ruby code using cane,
+# reek, flog, flay and rubocop and makes sure your numbers don't get
+# any worse over time.
 module Quality
   private
 
