a835e10777a9d7fc99e3f1bdbd831cbe3446ea24
Andy Waite <github.aw@andywaite.com>
2014-07-12 12:48:32 +0200

    Modularize quality checks

---
+++ lib/quality/flay.rb
@@ -0,0 +1,17 @@
+module Quality
+  module Flay
+    def quality_flay
+      private
+
+      ratchet_quality_cmd('flay',
+                          args: "-m 75 -t 99999 #{ruby_files}",
+                          emacs_format: true) do |line|
+        if line =~ /^[0-9]*\).* \(mass = ([0-9]*)\)$/
+          Regexp.last_match[1].to_i
+        else
+          0
+        end
+      end
+    end
+  end
+end
---
+++ lib/quality/rubocop.rb
@@ -0,0 +1,27 @@
+module Quality
+  module Rubocop
+    private
+
+    def quality_rubocop
+      ratchet_quality_cmd('rubocop',
+                          gives_error_code_on_violations: true,
+                          args: "--format emacs #{ruby_files}") do |line|
+        self.class.count_rubocop_violations(line)
+      end
+    end
+
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def count_rubocop_violations(line)
+        if line =~ /^.* file[s|] inspected, (.*) offence[s|] detected$/
+          0
+        else
+          1
+        end
+      end
+    end
+  end
+end
