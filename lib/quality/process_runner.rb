4c7b806e843e7be7e156125e64e1f1d523f81a8b
Vince Broz <vince@broz.cc>
2014-07-20 21:46:04 -0400

    Put ProcessRunner into a module

--- lib/quality/process_runner.rb
+++ lib/quality/process_runner.rb
@@ -1,17 +1,19 @@
 require 'English'
 
-class ProcessRunner
-  def initialize(full_cmd,
-                 popener: IO)
-    @full_cmd = full_cmd
-    @popener = popener
-  end
-
-  def run(&block)
-    command_output = nil
-    @popener.popen(@full_cmd) do |file|
-      yield file
+module Quality
+  class ProcessRunner
+    def initialize(full_cmd,
+                   popener: IO)
+      @full_cmd = full_cmd
+      @popener = popener
     end
-    $CHILD_STATUS.exitstatus
+
+    def run(&block)
+      command_output = nil
+      @popener.popen(@full_cmd) do |file|
+        yield file
+      end
+      $CHILD_STATUS.exitstatus
+    end
   end
 end
