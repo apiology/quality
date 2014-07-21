b67d228d513f6c1f33bff38204544c2a9f19811d
Vince Broz <vince@broz.cc>
2014-07-20 21:46:34 -0400

    Add test for ProcessRunner

---
+++ test/unit/test_process_runner.rb
@@ -0,0 +1,25 @@
+class TestProcessRunner < MiniTest::Unit::TestCase
+
+  def test_run
+    cmd = mock('cmd')
+    file = mock('file')
+    process_runner = get_test_object(cmd) do
+      @mocks[:popener].expects(:popen).with(cmd).yields(file)
+    end
+    process_runner.run do |file_yielded|
+      assert_equal(file, file_yielded)
+    end
+  end
+
+  def test_mocks
+    {
+      popener: mock('popener'),
+    }
+  end
+
+  def get_test_object(cmd, &twiddle_mocks)
+    @mocks = test_mocks
+    yield unless twiddle_mocks.nil?
+    Quality::ProcessRunner.new(cmd, @mocks)
+  end
+end
