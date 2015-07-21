8bafda7163abfd008deb210088645c9eaf605ad5
Vince Broz <vince@broz.cc>
2015-07-20 20:22:45 -0500

    Remove redundant documentation

--- lib/quality/rake/config.rb
+++ lib/quality/rake/config.rb
@@ -6,62 +6,12 @@
 module Quality
   # Configuration for running quality tool
   class Config
-    # Name of quality task.
-    # Defaults to :quality.
-    attr_accessor :quality_name
+    # See README.md for documentation on these configuration parameters.
 
-    # Name of ratchet task.
-    # Defaults to :ratchet.
-    attr_accessor :ratchet_name
-
-    # Array of strings describing tools to be skipped--e.g., ["cane"]
-    #
-    # Defaults to []
-    attr_accessor :skip_tools
-
-    # Log command executation
-    #
-    # Defaults to false
-    attr_accessor :verbose
-
-    # Array of directory names which contain ruby files to analyze.
-    #
-    # Defaults to %w(app lib test spec feature), which translates to *.rb in
-    # the base directory, as well as those directories.
-    attr_writer :ruby_dirs
-
-    # Array of directory names which contain any type of source
-    # files to analyze.
-    #
-    # Defaults to the same as ruby_dirs
-    attr_writer :source_dirs
-
-    # Pick any extra files that are source files, but may not have
-    # extensions--defaults to %w(Rakefile Dockerfile)
-    attr_accessor :extra_files
-
-    # Pick any extra files that are Ruby source files, but may not have
-    # extensions--defaults to %w(Rakefile)
-    attr_accessor :extra_ruby_files
-
-    # Extensions for Ruby language files--defaults to 'rb,rake'
-    attr_accessor :ruby_file_extensions
-
-    # Extensions for all source files--defaults to
-    # 'rb,rake,swift,cpp,c,java,py,clj,cljs,scala,js,yml,sh,json'
-    attr_accessor :source_file_extensions
-
-    # Pipe-separated regexp string describing what to look for in
-    # files as 'todo'-like 'punchlist' comments.
-    #
-    # Defaults to 'XXX|TODO'
-    attr_accessor :punchlist_regexp
-
-    # Relative path to output directory where *_high_water_mark
-    # files will be read/written
-    #
-    # Defaults to .
-    attr_accessor :output_dir
+    attr_accessor :quality_name, :ratchet_name, :skip_tools, :verbose,
+                  :ruby_dirs, :source_dirs, :extra_files, :extra_ruby_files,
+                  :ruby_file_extensions, :source_file_extensions,
+                  :punchlist_regexp, :output_dir
 
     def ruby_dirs
       @ruby_dirs ||= %w(src app lib test spec feature)
