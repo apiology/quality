84813317120496063b2be347a083477cb03737da
Vince Broz <vince@broz.cc>
2015-12-27 15:40:38 -0500

    Give a diagnostic when JSCS not configured

--- lib/quality/tools/jscs.rb
+++ lib/quality/tools/jscs.rb
@@ -8,12 +8,21 @@ module Quality
         '-r unix ' + js_files_arr.join(' ')
       end
 
+      def jscs_check_configured
+        return if js_files_arr.empty?
+
+        puts 'No JSCS config found!  To generate one, run ' \
+             'jscs --autoconfigure ' + js_files_arr.join(' ')
+        0
+      end
+
       def quality_jscs
         ratchet_quality_cmd('jscs',
                             args: jscs_args,
                             gives_error_code_on_violations: true) do |line|
-          if line =~ /^.*:\d+:\d+: /
-            1
+          return 1 if line =~ /^.*:\d+:\d+: /
+          if line =~ /^No configuration found/
+            jscs_check_configured
           else
             0
           end
