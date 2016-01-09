3c6b339f8321893c70bd35803094bd2e7f04c4cb
Vince Broz <vince@broz.cc>
2016-01-09 11:07:43 -0500

    Hopefully fix deprecated syntax

--- test/unit/test_helper.rb
+++ test/unit/test_helper.rb
@@ -1,10 +1,9 @@
 require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter =
-  SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter,
-  ]
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+]
 
 SimpleCov.start do
   add_filter 'vendor' # Don't include vendored stuff
