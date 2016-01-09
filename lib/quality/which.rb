117f71af6665da31db40c2b539d9df303d252286
Vince Broz <vince@broz.cc>
2016-01-09 13:54:12 -0500

    Improve coverage

--- lib/quality/which.rb
+++ lib/quality/which.rb
@@ -1,14 +1,25 @@
 # http://stackoverflow.com/questions/2108727/
 #   which-in-ruby-checking-if-program-exists-in-path-from-ruby
-class Which
-  def which(cmd)
-    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exts.each do |ext|
-        exe = File.join(path, "#{cmd}#{ext}")
-        return exe if File.executable?(exe) && !File.directory?(exe)
-      end
+module Quality
+  # Determine where a given executable lives, like the UNIX 'which' command
+  class Which
+    def initialize(env: ENV,
+                   file: File,
+                   separator: File::PATH_SEPARATOR)
+      @env = env
+      @file = file
+      @separator = separator
     end
-    nil
+
+    def which(cmd)
+      exts = @env['PATHEXT'] ? @env['PATHEXT'].split(';') : ['']
+      @env['PATH'].split(@separator).each do |path|
+        exts.each do |ext|
+          exe = @file.join(path, "#{cmd}#{ext}")
+          return exe if @file.executable?(exe) && !@file.directory?(exe)
+        end
+      end
+      nil
+    end
   end
 end
---
+++ test/unit/test_which.rb
@@ -0,0 +1,69 @@
+require_relative 'test_helper'
+
+# Test the Which class
+class TestWhich < MiniTest::Test
+  let_mock :cmd, :pathext, :dirname,
+           :foo_potential_bin_path,
+           :bar_potential_bin_path,
+           :baz_potential_bin_path
+
+  def expect_path_queried(dirname, binpath, executable, directory,
+                          extension = '')
+    @mocks[:file].expects(:join).with(dirname, "#{cmd}#{extension}")
+      .returns(binpath)
+    @mocks[:file].expects(:executable?).with(binpath)
+      .returns(executable)
+    @mocks[:file].expects(:directory?).with(binpath)
+      .returns(directory)
+      .at_least(0)
+  end
+
+  def mock_unix
+    @mocks[:env] = {
+      'PATH' => 'foo:bar:baz'
+    }
+    [
+      ['foo', foo_potential_bin_path, true, true],
+      ['bar', bar_potential_bin_path, false, nil],
+      ['baz', baz_potential_bin_path, true, false],
+    ].each do |dirname, binpath, executable, directory|
+      expect_path_queried(dirname, binpath, executable, directory)
+    end
+  end
+
+  def mock_windows
+    @mocks[:env] = {
+      'PATHEXT' => '.COM;.EXE;.BAT',
+      'PATH' => 'foo;bar;baz',
+    }
+    expect_path_queried('foo', foo_potential_bin_path, true, false, '.COM')
+  end
+
+  def test_which_unix
+    which = get_test_object(':') do
+      mock_unix
+    end
+    assert_equal(baz_potential_bin_path, which.which(cmd))
+  end
+
+  def test_which_windows
+    which = get_test_object(';') do
+      mock_windows
+    end
+    assert_equal(foo_potential_bin_path, which.which(cmd))
+  end
+
+  def create_mocks(separator)
+    {
+      env: mock('env'),
+      file: mock('file'),
+      separator: separator,
+    }
+  end
+
+  def get_test_object(separator, &twiddle_mocks)
+    @mocks = create_mocks(separator)
+    yield unless twiddle_mocks.nil?
+    Quality::Which.new(@mocks)
+  end
+end
