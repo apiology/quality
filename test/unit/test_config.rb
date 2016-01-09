32cf0e048228039ee5a319a8ea6535f15be4027c
Vince Broz <vince@broz.cc>
2016-01-09 16:25:06 -0500

    More changes on file extensions

--- test/unit/test_config.rb
+++ test/unit/test_config.rb
@@ -54,13 +54,13 @@ class TestConfig < MiniTest::Test
     t.source_files_exclude_glob =
       '{lib/whatever/imported_file.rb,lib/vendor/**/*.rb}'
 
-    # Extensions for Ruby language files--defaults to 'rb,rake'
+    # Extensions for Ruby language files--defaults to rb,rake
     t.ruby_file_extensions_arr = %w(rb rake)
 
     # Extensions for all source files--defaults to
-    # 'rb,rake,swift,cpp,c,java,py,clj,cljs,scala,js,yml,sh,json'
-    t.source_file_extensions_glob =
-      'rb,rake,swift,cpp,c,java,py,clj,cljs,scala,js,yml,sh,json'
+    # rb,rake,swift,cpp,c,java,py,clj,cljs,scala,js,yml,sh,json
+    t.source_file_extensions_arr =
+      %w(rb rake swift cpp c java py clj cljs scala js yml sh json)
 
     # Relative path to output directory where *_high_water_mark
     # files will be read/written
