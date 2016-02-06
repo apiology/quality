afceb6f54c9ab57d67bb823bd5c22e0cd983fc7a
Vince Broz <vince@broz.cc>
2016-02-06 17:43:25 -0500

    Add missing class

---
+++ test/unit/tool_tests/mock_runner.rb
@@ -0,0 +1,32 @@
+# Acts as the 'Runner' class for testing the tool plug-in modules
+class MockRunner
+  include Quality::Tools::Pep8
+  include Quality::Tools::Jscs
+  def initialize(lines)
+    @lines = lines
+  end
+
+  attr_accessor :sum, :lines
+
+  def python_files
+    []
+  end
+
+  def js_files_arr
+    []
+  end
+
+  def ratchet_quality_cmd(name,
+                          options,
+                          &block)
+    @sum = 0
+    lines.each do |line|
+      @sum += block.call(line)
+    end
+  end
+
+  def run(tool_name)
+    method("quality_#{tool_name}").call
+    sum
+  end
+end
