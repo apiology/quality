4caf9bdb55fb6b95b21d001f0225d3f55573fbad
Vince Broz <vince@broz.cc>
2016-08-21 23:17:00 -0400

    Don't cache

--- publish-docker-image.sh
+++ publish-docker-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 QUALITY_GEM_VERSION=$(gem search quality | grep ^'quality ' | cut -d'(' -f2 | cut -d')' -f1)
-docker build ${QUALITY_BUILD_ARGS} -t apiology/quality:${QUALITY_GEM_VERSION} .
+docker build --no-cache ${QUALITY_BUILD_ARGS} -t apiology/quality:${QUALITY_GEM_VERSION} .
 docker tag apiology/quality:${QUALITY_GEM_VERSION} apiology/quality:latest
 docker push apiology/quality:${QUALITY_GEM_VERSION}
 docker push apiology/quality:latest
