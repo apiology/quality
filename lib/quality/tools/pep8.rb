1234572397a4d0fbdc510e2586dc2774421def0e
Vince Broz <vince@broz.cc>
2016-08-28 11:31:02 -0400

    Fix RuboCop problem

--- lib/quality/tools/pep8.rb
+++ lib/quality/tools/pep8.rb
@@ -8,23 +8,27 @@ module Quality
         python_files.join(' ')
       end
 
+      def pep8_count_errors(line)
+        if line =~ /^Usage:/
+          # no files specified
+          0
+        elsif line =~ /^pep8: /
+          # no files specified
+          0
+        elsif line =~ /^$/
+          # no files specified
+          0
+        else
+          1
+        end
+      end
+
       def quality_pep8
         ratchet_quality_cmd('pep8',
                             args: pep8_args,
                             gives_error_code_on_no_relevant_code:
                               true) do |line|
-          if line =~ /^Usage:/
-            # no files specified
-            0
-          elsif line =~ /^pep8: /
-            # no files specified
-            0
-          elsif line =~ /^$/
-            # no files specified
-            0
-          else
-            1
-          end
+          pep8_count_errors(line)
         end
       end
     end
