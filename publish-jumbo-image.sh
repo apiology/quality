4a12a384ec2c95352c8e90747f399485d8bc11e6
Vince Broz <vince@broz.cc>
2016-09-24 08:12:56 -0400

    Shellcheck improvement

--- publish-jumbo-image.sh
+++ publish-jumbo-image.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-docker build -f Dockerfile.jumbo --no-cache ${QUALITY_BUILD_ARGS} -t apiology/quality:jumbo-${QUALITY_GEM_VERSION} .
-docker tag apiology/quality:jumbo-${QUALITY_GEM_VERSION} apiology/quality:jumbo-latest
-docker tag apiology/quality:jumbo-${QUALITY_GEM_VERSION} apiology/quality:jumbo-${QUALITY_GEM_MAJOR_VERSION}
-docker tag apiology/quality:jumbo-${QUALITY_GEM_VERSION} apiology/quality:jumbo-${QUALITY_GEM_MINOR_VERSION}
-docker push apiology/quality:jumbo-${QUALITY_GEM_VERSION}
-docker push apiology/quality:jumbo-${QUALITY_GEM_MAJOR_VERSION}
-docker push apiology/quality:jumbo-${QUALITY_GEM_MINOR_VERSION}
+# CACHE=
+CACHE=--no-cache
+docker build -f Dockerfile.jumbo ${CACHE} ${QUALITY_BUILD_ARGS} -t apiology/quality:jumbo-${QUALITY_GEM_VERSION:?} .
+docker tag apiology/quality:jumbo-${QUALITY_GEM_VERSION:?} apiology/quality:jumbo-latest
+docker tag apiology/quality:jumbo-${QUALITY_GEM_VERSION:?} apiology/quality:jumbo-${QUALITY_GEM_MAJOR_VERSION:?}
+docker tag apiology/quality:jumbo-${QUALITY_GEM_VERSION:?} apiology/quality:jumbo-${QUALITY_GEM_MINOR_VERSION:?}
+docker push apiology/quality:jumbo-${QUALITY_GEM_VERSION:?}
+docker push apiology/quality:jumbo-${QUALITY_GEM_MAJOR_VERSION:?}
+docker push apiology/quality:jumbo-${QUALITY_GEM_MINOR_VERSION:?}
 docker push apiology/quality:jumbo-latest
 
