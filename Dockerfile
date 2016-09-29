62d6ed26fd7057278146f60ac4b8ad2fda903a73
Vince Broz <broz@macmicro.fios-router.home>
2016-09-28 22:58:20 -0400

    Use Rocker to simplify Dockerfiles

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM alpine:latest
-RUN apk update && apk add --no-cache ruby ruby-irb ruby-dev make gcc libc-dev git icu-dev zlib-dev g++ cmake && gem install --no-ri --no-rdoc quality io-console bigdecimal rake
-VOLUME /usr/app
-RUN mkdir /usr/quality
-ADD sample-project/Rakefile /usr/quality
-WORKDIR /usr/app
-COPY entrypoint.sh /
-ENTRYPOINT /entrypoint.sh
--- publish-docker-image.sh
+++ publish-docker-image.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
-export QUALITY_GEM_VERSION=$(gem search quality | grep ^'quality ' | cut -d'(' -f2 | cut -d')' -f1)
-export QUALITY_GEM_MAJOR_VERSION=$(echo ${QUALITY_GEM_VERSION} | cut -d '.' -f1)
-export QUALITY_GEM_MINOR_VERSION=$(echo ${QUALITY_GEM_VERSION} | cut -d '.' -f1-2)
+QUALITY_GEM_VERSION=$(gem search quality | grep ^'quality ' | cut -d'(' -f2 | cut -d')' -f1)
+QUALITY_GEM_MAJOR_VERSION=$(echo "${QUALITY_GEM_VERSION}" | cut -d '.' -f1)
+QUALITY_GEM_MINOR_VERSION=$(echo "${QUALITY_GEM_VERSION}" | cut -d '.' -f1-2)
 
-./publish-skinny-image.sh
-
-./publish-jumbo-image.sh
+rocker build \
+       -var "quality_gem_version=${QUALITY_GEM_VERSION:?}" \
+       -var "quality_gem_major_version=${QUALITY_GEM_MAJOR_VERSION:?}" \
+       -var "quality_gem_minor_version=${QUALITY_GEM_MINOR_VERSION:?}" \
+       --push \
+       ${QUALITY_BUILD_ARGS} \
