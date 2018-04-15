aa52de9c65a1ff8f7792fdf1c8175ca998537839
Bundle updater <circleci@broz.cc>
2018-04-15 19:45:40 -0400

    Drop absolute paths from Docker tool outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+set -o pipefail
+
 gem update --no-document brakeman
 bundle-audit update
-rake -f /usr/quality/Rakefile "$@"
+# drop absolute paths from tool output
+rake -f /usr/quality/Rakefile "$@" 2>&1 | sed -e 's@/usr/app/@@g'
