6de41293055d59e110f23698f1cd3607aaefb81c
Bundle updater <circleci@broz.cc>
2018-04-15 20:00:44 -0400

    Only filter at start of line

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -o pipefail
 gem update --no-document brakeman
 bundle-audit update
 # drop absolute paths from tool output
-rake -f /usr/quality/Rakefile "$@" 2>&1 | sed -e 's@/usr/app/@@g'
+rake -f /usr/quality/Rakefile "$@" 2>&1 | sed -e 's@^/usr/app/@@g'
