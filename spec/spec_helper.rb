99e398add6610484db591e446007b6e6dd400eed
Vince Broz <vince@broz.cc>
2020-01-01 22:12:58 -0500

    Include in Coveraalls coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'coveralls'
 
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+]
 SimpleCov.start do
   # this dir used by TravisCI
   add_filter 'vendor'
