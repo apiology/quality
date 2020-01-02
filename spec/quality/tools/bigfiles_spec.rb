16156aecba20a55526abce74a235a17a0bfbc128
Vince Broz <vince@broz.cc>
2020-01-01 21:44:13 -0500

    RuboCop fixes

--- spec/quality/tools/bigfiles_spec.rb
+++ spec/quality/tools/bigfiles_spec.rb
@@ -2,5 +2,13 @@
 
 require_relative '../../spec_helper'
 require 'quality/tools/bigfiles'
+require 'quality/runner'
 
-describe Quality::Tools::Bigfiles do; end
+describe Quality::Tools::Bigfiles do
+  let(:runner) { instance_double(Quality::Runner) }
+  let(:bigfiles) { described_class.new(runner) }
+
+  it 'can be created' do
+    expect(bigfiles).not_to be(nil)
+  end
+end
