9ff6fd039ab6a779230f1fed2e7fd1354665bea5
Vince Broz <vince@broz.cc>
2022-11-26 22:18:51 -0500

    Add nocov markers

--- lib/quality/version.rb
+++ lib/quality/version.rb
@@ -3,6 +3,8 @@
 # Quality is a tool that runs quality checks on Ruby code using cane,
 # reek, flog, flay and rubocop and makes sure your numbers don't get
 # any worse over time.
+# :nocov:
 module Quality
   VERSION = '40.0.0'
 end
+# :nocov:
